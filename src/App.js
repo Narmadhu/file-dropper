21ad2922f904006d13f7562e26d158c1841fb487
Narmadhu <oknarmadhu@gmail.com>
2020-10-27 17:26:57 +0530

    added file-drop box

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import  React  from "react";
 import './App.css';
 import DropBox from "./DropBox"
-import {Button, button} from "react-bootstrap"
+
 
 function App() {
   return (
-    <div className="App">
+    <div className="app" >
      <DropBox />
     </div>
   );
--- src/DropBox.js
+++ src/DropBox.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import "./DropBox.css"
+import {Button, Card} from "react-bootstrap"
+
 
 function DropBox() {
     return (
-        <div>
-            
+        <div className="drop-box" >
+          <Card>  
+          <form>
+           <input type="text" className="invisible-input" />
+            <img src="/images/images.jpg" className="file-add" />
+            <input type="file" class="file-input" multiple></input>   
+            <Card.Title className="file-heading">Drop your file</Card.Title>
+            <Button className="file-btn" > Or Select your file </Button>
+          </form>
+          </Card>  
         </div>
     )
 }
